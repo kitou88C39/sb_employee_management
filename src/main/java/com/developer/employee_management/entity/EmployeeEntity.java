c88fbe2dea47ab0e9b6de9cef8d5137cccb8a699
kitoumasaomi <kanfutrooper@gmail.com>
2024-08-12 22:19:02 +0900

    java/com.developer/employee_managementにentityパッケージとEmployeeEntity.javaを作成する

--- src/main/java/com/developer/employee_management/entity/EmployeeEntity.java
+++ src/main/java/com/developer/employee_management/entity/EmployeeEntity.java
@@ -18,4 +18,14 @@ public class EmployeeEntity {
         this.address = address;
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
 }
