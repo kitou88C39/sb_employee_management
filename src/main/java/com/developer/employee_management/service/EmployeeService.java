98dbb75716b2f1d95d9ba6a78afb90b6e738d3de
kitoumasaomi <kanfutrooper@gmail.com>
2024-08-13 16:09:56 +0900

    java/com.developer/employee_managementにserviceパッケージと EmployeeService.javaを作成する

--- src/main/java/com/developer/employee_management/service/EmployeeService.java
+++ src/main/java/com/developer/employee_management/service/EmployeeService.java
@@ -1,5 +1,9 @@
 package com.developer.employee_management.service;
 
+import com.developer.employee_management.entity.EmployeeEntity;
+import java.util.List;
+
 public class EmployeeService {
+    List<EmployeeEntity> findAllEmployee();
 
 }
