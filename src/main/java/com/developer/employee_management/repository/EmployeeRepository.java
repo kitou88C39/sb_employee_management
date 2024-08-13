8cca897d8a4a956fb7c59d8fcda364a9ec162fc7
kitoumasaomi <kanfutrooper@gmail.com>
2024-08-13 15:20:49 +0900

    java/com.developer/employee_managementにserviceパッケージと EmployeeService.javaを作成する

--- src/main/java/com/developer/employee_management/repository/EmployeeRepository.java
+++ src/main/java/com/developer/employee_management/repository/EmployeeRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface EmployeeRepository extends JpaRepository<EmployeeEntity> {
+public interface EmployeeRepository extends JpaRepository<EmployeeEntity, Long> {
 
 }
---
+++ src/main/java/com/developer/employee_management/service/EmployeeService.java
@@ -0,0 +1,5 @@
+package com.developer.employee_management.service;
+
+public class EmployeeService {
+
+}
