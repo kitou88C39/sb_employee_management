792d5a1ab69fe6c6d1eea26bc4b9981ce3677188
kitoumasaomi <kanfutrooper@gmail.com>
2024-08-13 21:42:27 +0900

    java/com.developer/employee_managementにcontrollerパッケージとEmployeeController.javaを作成する

--- src/main/java/com/developer/employee_management/controller/EmployeeController.java
+++ src/main/java/com/developer/employee_management/controller/EmployeeController.java
@@ -3,8 +3,12 @@ package com.developer.employee_management.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.developer.employee_management.service.EmployeeService;
+
 @RestController
 @RequestMapping("/employee")
 public class EmployeeController {
+
+    private final EmployeeService employeeService;
 
 }
